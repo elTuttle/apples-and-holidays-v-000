37f4b71131748311ba20cecffca9b82de0225962
John Tuttle <thejwt@gmail.com>
2017-09-27 01:29:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts season.capitalize!
-    season.each do |holiday, supplies|
-      puts "#{holiday.capitalize!}: #{supplies.join(", ")}"
+    puts season.capitalize
+    season.each do |holiday, supplies| 
+      puts "#{holiday.capitalize}: #{supplies.join(", ")}"
     end
   end
 
