2b170af6669a678ca21da6c6b9e25835f0832c83
John Tuttle <thejwt@gmail.com>
2017-09-27 01:36:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday = holiday.capitalize!
       holiday = holiday.to_s.split("_")
       holiday = holiday.join(" ")
-      puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
+      puts "  #{holiday}: #{supplies.join(", ")}"
     end
   end
 
